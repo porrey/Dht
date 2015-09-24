24110666dc9bd13b99a5ebf9990162122d258180
Daniel Porrey <Daniel.Porrey@hotmail.com>
2015-09-24 07:43:31 -0500

    Added the PDF datasheet fo the DHT11.

--- source/Winodws 10 IoT Core/DHT Solution/Sensors.Dht/Dht11.cpp
+++ source/Winodws 10 IoT Core/DHT Solution/Sensors.Dht/Dht11.cpp
@@ -1,4 +1,21 @@
-﻿#include "pch.h"
+﻿// Copyright © 2015 Daniel Porrey
+//
+// This file is part of the DHT11 Solution.
+// 
+// DHT11 Solution is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// DHT11 Solution is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with DHT11 Solution. If not, see http://www.gnu.org/licenses/.
+//
+#include "pch.h"
 #include "Dht11.h"
 #include <ppltasks.h>
 
