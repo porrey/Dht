28b9274b9fff1df1930f5fcad3ba9b2b01fb0fb3
Daniel Porrey <porrey@users.noreply.github.com>
2022-08-29 21:23:14 -0500

    Updated references.

--- Src/Windows IoT/DHT Solution/Sensors.OneWire/Common/BindablePage.cs
+++ Src/Windows IoT/DHT Solution/Sensors.OneWire/Common/BindablePage.cs
@@ -1,19 +1,19 @@
-﻿// Copyright © 2015 Daniel Porrey
+﻿// Copyright © 2015-2022 Daniel Porrey
 //
-// This file is part of the DHT11 Solution.
+// This file is part of the Sensors.OneWire Solution.
 // 
-// DHT11 Solution is free software: you can redistribute it and/or modify
+// Sensors.OneWire Solution is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// DHT11 Solution is distributed in the hope that it will be useful,
+// Sensors.OneWire Solution is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
-// along with DHT11 Solution. If not, see http://www.gnu.org/licenses/.
+// along with Sensors.OneWire Solution. If not, see http://www.gnu.org/licenses/.
 //
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
--- Src/Windows IoT/Samples/Dht-Multiple/Dht-Multi-Sample/MainPage.xaml.cs
+++ Src/Windows IoT/Samples/Dht-Multiple/Dht-Multi-Sample/MainPage.xaml.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// Copyright © 2015-2022 Daniel Porrey
+//
+// This file is part of the Dht-Multi-Sample Solution.
+// 
+// Dht-Multi-Sample Solution is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Dht-Multi-Sample Solution is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Dht-Multi-Sample Solution. If not, see http://www.gnu.org/licenses/.
+//
+using System;
 using System.Threading.Tasks;
 using Dht.Multi.Sample.Common;
 using Sensors.Dht;
